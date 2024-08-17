3fb94042b9049a11883e118488a84a11b4b19662
Lastonetry <alwaysoptimisticfutureiitian@gmail.com>
2024-08-17 07:38:01 +0530

    Add files via upload

---
+++ jiosaavn/config/settings.py
@@ -0,0 +1,15 @@
+from os import getenv
+
+API_ID = getenv("API_ID", "20937630")
+API_HASH = getenv("API_HASH", "3290b11b9246bff1ff44f13e9c7ffb45")
+BOT_TOKEN = getenv("BOT_TOKEN", "7366817195:AAHx2as_r3xxCG63lJ-ugdHihwVL_2xzGsA")
+BOT_COMMANDS = (
+    ("start", "Initialize the bot and check its status"),
+    ("settings", "Configure and manage bot settings"),
+    ("help", "Get information on how to use the bot"),
+    ("about", "Learn more about the bot and its features"),
+)
+
+DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://tabolo8539:0evqZDV4fC5fD17c@cluster0.cw8zxus.mongodb.net/?retryWrites=true&w=majority")
+HOST = getenv("HOST", "0.0.0.0")
+PORT = int(getenv("PORT", "8080"))
